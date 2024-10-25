234a9be6ed159a0ced07d79048343a41229579db
blenAZ <blenzeru27@gmail.com>
2024-10-25 17:04:16 +0300

    Save local changes in AppNavigator.js

--- NPS/navigation/AppNavigator.js
+++ NPS/navigation/AppNavigator.js
@@ -1,73 +1,65 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from '../screens/LoginScreen';
-import RegisterScreen from '../screens/RegisterScreen';
 import HomeScreen from '../screens/HomeScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import LoginScreen from '../screens/LoginScreen';
 import RequestScreen from '../screens/RequestScreen';
 import BookingScreen from '../screens/BookingScreen';
 import ChatScreen from '../screens/ChatScreen';
-import ProviderHomeScreen from '../screens/ProviderHomeScreen';
-import ManageRequestsScreen from '../screens/ManageRequestsScreen';
-import ManageBookingsScreen from '../screens/ManageBookingsScreen';
-import PaymentHistoryScreen from '../screens/PaymentHistoryScreen';
+import InfoScreen from '../screens/InfoScreen';
+import ProfileScreen from '../screens/ProfileScreen'; // Import ProfileScreen
+import TransactionsScreen from '../screens/TransactionsScreen'; // Import TransactionsScreen
 
 const Stack = createStackNavigator();
-
 const AppNavigator = () => {
   return (
-    
     <Stack.Navigator>
-      {/* Hide the default header for all screens */}
-       <Stack.Screen 
-        name="Login" 
-        component={LoginScreen} 
-        options={{ headerShown: false }} 
+      <Stack.Screen
+        name="Register"
+        component={RegisterScreen}
+        options={{ headerShown: false }}
       />
-      <Stack.Screen 
-        name="Register" 
-        component={RegisterScreen} 
-        options={{ headerShown: false }} 
-      /> 
-      <Stack.Screen 
-        name="Home" 
-        component={HomeScreen} 
-        options={{ headerShown: false }} 
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{ headerShown: false }}
       />
-      
-      <Stack.Screen 
-        name="Request" 
-        component={RequestScreen} 
-        options={{ headerShown: false }} 
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ headerShown: false }}
       />
-      <Stack.Screen 
-        name="Booking" 
-        component={BookingScreen} 
-        options={{ headerShown: false }} 
+      <Stack.Screen
+        name="Request"
+        component={RequestScreen}
+        options={{ headerShown: false }}
       />
-      <Stack.Screen 
-        name="Chat" 
-        component={ChatScreen} 
-        options={{ headerShown: false }} 
+      <Stack.Screen
+        name="Booking"
+        component={BookingScreen}
+        options={{ headerShown: false }}
       />
-      <Stack.Screen 
-        name="ProviderHome" 
-        component={ProviderHomeScreen} 
-        options={{ headerShown: false }} 
+      <Stack.Screen
+        name="Chat"
+        component={ChatScreen}
+        options={{ headerShown: false }}
       />
-      <Stack.Screen 
-        name="ManageRequests" 
-        component={ManageRequestsScreen} 
-        options={{ headerShown: false }} 
+      <Stack.Screen
+        name="Info"
+        component={InfoScreen}
+        options={{ headerShown: false }}
       />
-      <Stack.Screen 
-        name="ManageBookings" 
-        component={ManageBookingsScreen} 
-        options={{ headerShown: false }} 
+      {/* Adding TransactionsScreen to the stack */}
+      <Stack.Screen
+        name="Transactions"
+        component={TransactionsScreen}
+        options={{ headerShown: false }}
       />
-      <Stack.Screen 
-        name="PaymentHistory" 
-        component={PaymentHistoryScreen} 
-        options={{ headerShown: false }} 
+      {/* Adding ProfileScreen to the stack */}
+      <Stack.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{ headerShown: false }}
       />
     </Stack.Navigator>
   );
